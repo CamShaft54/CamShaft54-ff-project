4eb677205ed955534c5fe38b41f09f4c58f6cc98
Camshaft54 <cam.shaw1234@gmail.com>
2020-04-28 10:01:47 -0700

    Continue Pymunk tutorial Create Pyglet_testing.py

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,4 +1,14 @@
 import pymunk
+import time
 
 space = pymunk.Space()
 space.gravity = 0, -1000
+body = pymunk.Body(1, 1666)
+body.position = 50, 100
+
+space.add(body)
+
+while True:
+    space.step(0.02)  # 50 fps
+    print(body.position)
+    time.sleep(0.5)
