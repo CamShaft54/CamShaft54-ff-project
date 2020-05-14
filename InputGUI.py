b05f841811040f7ca5bfb0108bf7529d6bac21ad
camshaft54 <cam.shaw1234@gmail.com>
2020-05-14 11:44:51 -0700

    Rename matplotlib_testing.py to InputGUI.py

---
+++ InputGUI.py
@@ -0,0 +1,7 @@
+import PySimpleGUI as Sg
+
+layout = [
+    [Sg.Text("Welcome to Softballs in the Gym Day Simulator!")],
+    [Sg.Text("In this simulator you can specify the dimensions of a gym and the size of the softball to run a visual simulation.")],
+    [Sg.Text("Here are the keyboard shortcuts for the simulator:")]
+]
--- matplotlib_testing.py
+++
@@ -1,4 +0,0 @@
-list1 = [1, 2, 3, 4, 5, 6, 7, 8]
-list2 = [1, 2, 3, 4, 5, 6]
-
-print(min(len(list1), len(list2)))
